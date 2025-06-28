829a62d84ee4d7fa97f8e405ed44d5585c501694
scrxpie <elsenn3345@gmail.com>
2025-06-28 22:03:26 +0400

    Update PartnerGuild.js

--- models/PartnerGuild.js
+++ models/PartnerGuild.js
@@ -7,7 +7,11 @@ const partnerGuildSchema = new mongoose.Schema({
   partnerChannelId: String,
   autoApprove: { type: Boolean, default: false },
   partnerCooldown: { type: Number, default: 4 },
-  lastPartneredAt: Date
+  lastPartneredAt: Date,
+  approved: { type: Boolean, default: false },
+  reviewed: { type: Boolean, default: false },
+  admins: [String],        // Yetkililer userID olarak burada tutulacak
+  logChannelId: String     // Başvuruların gönderileceği kanal
 });
 
 module.exports = mongoose.model('PartnerGuild', partnerGuildSchema);
